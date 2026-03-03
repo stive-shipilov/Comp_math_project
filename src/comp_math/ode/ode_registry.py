26f7132e05d505f8f9bcfa5acf1fb7256ae2bf67
Shipilov Stepan <stive.shipilov@yandex.ru>
2026-03-03 11:37:36 -0800

    fix: опечатка в названии файла

--- src/comp_math/ode/ode_registry.py
+++ src/comp_math/ode/ode_registry.py
@@ -1,6 +1,6 @@
 from typing import Dict, Type
 from ..core.base_method_registry import BaseMethodRegistry
-from ..ode.base_ode_solver import BaseODESolver
+from .base_ode_solver import BaseODESolver
 
 class ODERegistry(BaseMethodRegistry):
     """Фабрика для создания экземпляров решателей ОДУ"""
