22393808dcdf2bbfbf59c10252dde03beb5bd381
Shipilov Stepan <stive.shipilov@yandex.ru>
2026-03-03 19:57:45 -0800

    feat: добавлены методы в фабрику

--- src/comp_math/ode/ode_registry.py
+++ src/comp_math/ode/ode_registry.py
@@ -1,10 +1,13 @@
 from typing import Dict, Type
+
+from comp_math.ode.explicit.multistep.adams_impl import (AdamsBashforth1Solver, AdamsBashforth2Solver, 
+AdamsBashforth3Solver, AdamsBashforth4Solver)
 from ..core.base_method_registry import BaseMethodRegistry
 from .base_ode_solver import BaseODESolver
-from .explicit.euler_ode_solver import EulerODESolver
-from .explicit.heun_ode_solver import HeunODESolver
-from .explicit.kutta_third_ode_solver import KuttaThirdODESolver
-from .explicit.rk4_ode_solver import Rk4ODESolver
+from .explicit.single_step.euler_ode_solver import EulerODESolver
+from .explicit.single_step.heun_ode_solver import HeunODESolver
+from .explicit.single_step.kutta_third_ode_solver import KuttaThirdODESolver
+from .explicit.single_step.rk4_ode_solver import Rk4ODESolver
 
 class ODERegistry(BaseMethodRegistry):
     """Фабрика для создания экземпляров решателей ОДУ"""
@@ -13,6 +16,10 @@ class ODERegistry(BaseMethodRegistry):
         "euler": EulerODESolver,
         "kutta": KuttaThirdODESolver,
         "heun": HeunODESolver,
-        "rk4": Rk4ODESolver
+        "rk4": Rk4ODESolver,
+        "adam1": AdamsBashforth1Solver,
+        "adam2": AdamsBashforth2Solver,
+        "adam3": AdamsBashforth3Solver,
+        "adam4": AdamsBashforth4Solver
 }
     
