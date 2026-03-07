5a9cd6188640e61c363e87c0267ba489ec9dd399
Shipilov Stepan <stive.shipilov@yandex.ru>
2026-03-07 11:40:54 -0800

    feat: использование собтвенной реализации СЛАУ солвера

--- src/comp_math/ode/implicit/base_rosenbrock_solver.py
+++ src/comp_math/ode/implicit/base_rosenbrock_solver.py
@@ -2,6 +2,7 @@ import numpy as np
 from typing import Callable, Optional, Tuple, List
 from comp_math.linear_algebra.objects.vector import Vector
 from comp_math.linear_algebra.objects.matrix import Matrix
+from comp_math.linear_algebra.sla_solvers.SLA_solvers_registry import SLASolverRegistry
 from comp_math.ode.base_ode_solver import BaseODESolver
 from comp_math.differentiation.numerical.numericalJacobian import NumericalJacobian
 
@@ -18,7 +19,7 @@ class BaseRosenbrockSolver(BaseODESolver):
         self.gamma_matrix = None
         self.order = None
     
-    def _numerical_jacobian(self, f: Callable, t: float, y: Vector) -> np.ndarray:
+    def _numerical_jacobian(self, f: Callable, t: float, y: Vector) -> Matrix:
         """Численное дифференцирование для получения матрицы Якоби"""
         def f_numpy(y_np: np.ndarray) -> np.ndarray:
             y_vec = Vector(y_np.tolist())
@@ -26,11 +27,10 @@ class BaseRosenbrockSolver(BaseODESolver):
         
         y_np = y.to_numpy()
         J_np = NumericalJacobian.differentiate(f_numpy, y_np, 1e-8)
-        return J_np
+        return Matrix(J_np.tolist())
     
     def solve(self, f: Callable, dim: int, t_span: Tuple[float, float], 
-              y0: List[float], h: float, 
-              jacobian: Optional[Callable] = None) -> Tuple[np.ndarray, np.ndarray]:
+              y0: List[float], h: float) -> Tuple[np.ndarray, np.ndarray]:
         """Решение системы ОДУ методом Розенброка"""
         
         t = np.arange(t_span[0], t_span[1] + h, h)
@@ -39,46 +39,39 @@ class BaseRosenbrockSolver(BaseODESolver):
         y0_vec = y0 if isinstance(y0, Vector) else Vector(y0)
         y[0] = y0_vec
         
-        I = np.eye(dim)
+        I = Matrix(np.eye(dim))
         n_stages = len(self.b)
                 
         for n in range(len(t) - 1):
             tn = t[n]
             yn = y[n]
             
-            if jacobian:
-                J_matrix = jacobian(tn, yn)
-                J = J_matrix.to_numpy() if isinstance(J_matrix, Matrix) else np.array(J_matrix)
-            else:
-                J = self._numerical_jacobian(f, tn, yn)
+            J = self._numerical_jacobian(f, tn, yn)
             
-            M = I - h * self.gamma * J
+            M = I.subtract(J * (h * self.gamma))
             k = []
             
             for i in range(n_stages):
                 y_arg = yn
                 for j in range(i):
-                    y_arg = y_arg + self.a[i][j] * k[j]
+                    y_arg = y_arg + k[j] * self.a[i][j]
                 
                 t_stage = tn + self.alpha[i] * h
                 f_val = f(t_stage, y_arg)
                 
-                rhs = h * f_val.to_numpy().copy()
-                sum_gamma_k = np.zeros(dim)
+                rhs = f_val * h
+                sum_gamma_k = Vector(np.zeros(dim))
                 for j in range(i):
-                    sum_gamma_k += self.gamma_matrix[i][j] * k[j].to_numpy()
-                rhs += h * J @ sum_gamma_k
+                    sum_gamma_k = sum_gamma_k + k[j] * self.gamma_matrix[i][j]
+                rhs = rhs + (J * sum_gamma_k) * h
                 
-                try:
-                    k_i_np = np.linalg.solve(M, rhs)
-                except np.linalg.LinAlgError:
-                    k_i_np = np.linalg.lstsq(M, rhs, rcond=None)[0]
-                
-                k.append(Vector(k_i_np.tolist()))
+                bcg_solver = SLASolverRegistry.create_solver("bcg")
+                k_i = bcg_solver.solve(M, rhs)
+                k.append(k_i)
             
             y_new = yn
             for i in range(n_stages):
-                y_new = y_new + self.b[i] * k[i]
+                y_new = y_new + k[i] * self.b[i]
             
             y[n+1] = y_new
         
