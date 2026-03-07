8116f54425fc3bdfd770e6c86ba9345aa8d33a13
Shipilov Stepan <stive.shipilov@yandex.ru>
2026-03-07 10:57:13 -0800

    fix: исправлен метод Гира

--- src/comp_math/ode/implicit/base_gear_solver.py
+++ src/comp_math/ode/implicit/base_gear_solver.py
@@ -2,6 +2,7 @@ import numpy as np
 from typing import Callable, Optional, Tuple, List
 from comp_math.linear_algebra.objects.vector import Vector
 from comp_math.linear_algebra.objects.matrix import Matrix
+from comp_math.linear_algebra.sla_solvers.SLA_solvers_registry import SLASolverRegistry
 from comp_math.ode.base_ode_solver import BaseODESolver
 from comp_math.nonlinear.nonlinear_solvers_registry import NewtonSolverND
 from comp_math.differentiation.numerical.numericalJacobian import NumericalJacobian
@@ -56,39 +57,31 @@ class BaseGearSolver(BaseODESolver):
             if t_current + current_h > t_span[1]:
                 current_h = t_span[1] - t_current
             
-            try:
-                J = self._get_jacobian(f, t_current, y[-1])
+            J = self._get_jacobian(f, t_current, y[-1])
+            
+            if len(y) <= self.order:
+                # Разгон
+                M = np.eye(dim) - current_h * J
+                f_val = f(t_current + current_h, y[-1])
+                rhs = y[-1].to_numpy() + current_h * f_val.to_numpy()
+                cg_solver = SLASolverRegistry.create_solver("cg")
+                y_new_np = cg_solver.solve(Matrix(M), Vector(rhs)).to_numpy()
+                y_new = Vector(y_new_np.tolist())
+            else:
+                # Основной метод Гира
+                y_prev = [y[-1 - j] for j in range(min(self.order, len(y)))]
+                y_pred = self._compute_prediction(y_prev)
+                G = self._create_bdf_function(f, t_current, current_h, y_pred)
                 
-                if len(y) <= self.order:
-                    # Разгон
-                    M = np.eye(dim) - current_h * J
-                    f_val = f(t_current + current_h, y[-1])
-                    rhs = y[-1].to_numpy() + current_h * f_val.to_numpy()
-                    # TODO: заменить на мою реализацю, так сейчас мои не справляются с 
-                    # большими числами в матрице
-                    y_new_np = np.linalg.solve(M, rhs)
-                    y_new = Vector(y_new_np.tolist())
-                else:
-                    # Основной метод Гира
-                    y_prev = [y[-1 - j] for j in range(min(self.order, len(y)))]
-                    y_pred = self._compute_prediction(y_prev)
-                    G = self._create_bdf_function(f, t_current, current_h, y_pred)
-                    
-                    def J_func(y_np: np.ndarray) -> np.ndarray:
-                        return np.eye(dim) - self.beta * current_h * J
-                    
-                    x0 = y_pred.to_numpy()
-                    y_new_np = self.newton_solver.solve(F=G, x0=x0, J=J_func)
-                    y_new = Vector(y_new_np.tolist())
+                def J_func(y_np: np.ndarray) -> np.ndarray:
+                    return np.eye(dim) - self.beta * current_h * J
                 
-                t.append(t_current + current_h)
-                y.append(y_new)
-                t_current += current_h
-                
-            # Адаптивное уменьшение щага, на случай, если переданного h не хваьтило 
-            except Exception:
-                current_h *= 0.5
-                if current_h < 1e-12:
-                    break
-        
+                x0 = y_pred.to_numpy()
+                y_new_np = self.newton_solver.solve(F=G, x0=x0, J=J_func)
+                y_new = Vector(y_new_np.tolist())
+            
+            t.append(t_current + current_h)
+            y.append(y_new)
+            t_current += current_h
+            
         return np.array(t), np.array(y, dtype=object)
