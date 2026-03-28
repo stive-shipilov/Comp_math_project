0effc2ab48f462b0f8ea5f8173edceddac09b5ef
UsoltsevI <usoltsev-ivan2000@yandex.ru>
2026-03-28 12:56:17 +0300

    Метод стрельбы: чистка кода

--- src/comp_math/bvp/solvers/shooting.py
+++ src/comp_math/bvp/solvers/shooting.py
@@ -7,12 +7,12 @@ from comp_math.linear_algebra.objects.matrix import Matrix
 from comp_math.linear_algebra.objects.vector import Vector
 from comp_math.linear_algebra.sla_solvers.SLA_solvers_registry import SLASolverRegistry
 from comp_math.ode.ode_registry import ODERegistry
-import matplotlib.pyplot as plt
+
 
 class ShootingSolver(BaseBVPSolver):
     """Метод стрельбы для уравнения второго порядка"""
     
-    def __init__(self, problem: FirstOrderSystem):
+    def __init__(self, problem: FirstOrderSystem, ode_solver_name: str = "rk4", sla_solver_name: str = "gauss"):
         super().__init__(problem)
         
         if not isinstance(problem, FirstOrderSystem):
@@ -29,12 +29,13 @@ class ShootingSolver(BaseBVPSolver):
         self.vec_dim = self.n_vars * (self.n_vars + 1)
 
         self.jacobian = NumericalJacobian()
-        self.ode_solver = ODERegistry.create_solver("rk4")
-        self.sla_solver = SLASolverRegistry.create_solver("gauss")
+        self.ode_solver = ODERegistry.create_solver(ode_solver_name)
+        self.sla_solver = SLASolverRegistry.create_solver(sla_solver_name)
 
         # для графика невязок от итерации
         self.alpha_iters = []
         self.alphas = []
+        self.last_iteration = 0
     
     def _get_expected_type(self) -> str:
         return "first_order_system"
@@ -57,34 +58,34 @@ class ShootingSolver(BaseBVPSolver):
         
         return self.jacobian.differentiate(temp_bc_a, ya, h_jacobi), \
             self.jacobian.differentiate(temp_bc_b, yb, h_jacobi)
- 
-    def arr_to_matrix(self, arr: np.ndarray):
+
+    def arr_to_matrix(self, arr: np.ndarray) -> np.ndarray:
         return arr.reshape(self.n_vars, self.n_vars)
     
-    def matrix_to_arr(self, matrix: np.ndarray):
+    def matrix_to_arr(self, matrix: np.ndarray) -> np.ndarray:
         return matrix.flatten()
     
-    def get_y_from_vec(self, vec: np.ndarray):
+    def get_y_from_vec(self, vec: np.ndarray) -> np.ndarray:
         return vec[:self.n_vars]
     
-    def get_v_from_vec(self, vec: np.ndarray):
+    def get_v_from_vec(self, vec: np.ndarray) -> np.ndarray:
         """Достаёт v из vec = [y, v] и возвращает в виде матрицы nxn"""
         return self.arr_to_matrix(vec[self.n_vars:])
     
-    def get_initial_v(self):
-        # TODO: протестировать какая начальная матрица чаще всего самая выгодная;
-        # можно сделать матрицу из случайных числе или задаваемую пользователем
+    def get_initial_v(self) -> np.ndarray:
+        # это всегда единичная матрица на левом конце
         return np.eye(self.n_vars)
     
-    def get_alphas(self):
+    def get_alphas(self) -> Tuple[list[float], list[Vector]]:
         return self.alpha_iters, self.alphas
     
     def reset_alphas(self):
         self.alpha_iters = []
         self.alphas = []
+        self.last_iteration = 0
     
     def solve(self, a0: np.ndarray, h: float = 1e-3, h_jacobi = 1e-6, tol: float = 1e-6, max_iter: int = 50, \
-              verbose: bool = True) -> Tuple[np.ndarray, np.ndarray]:
+              record_alpha: bool = True) -> Tuple[np.ndarray, np.ndarray]:
         """Решает методом стрельбы."""
 
         self.reset_alphas()
@@ -114,26 +115,14 @@ class ShootingSolver(BaseBVPSolver):
 
         for i in range(0, max_iter):
             # vec = [y, v]
-            # print("i = ", i)
-            # print("a = ", a.to_numpy())
+
             # сигнатура вызываемой фнукции:
             # def solve(self, f, dim, t_span: Tuple[float, float], y0: List[float], h: float)
-            # print("self.vec_dim = ", self.vec_dim)
-            # print("self.vec_a = ", vec_a)
-            # print("new_system(vec_a) = ", new_system(self.a, vec_a))
             vec_a = np.append(a.to_numpy(), self.matrix_to_arr(self.get_initial_v()))
             x, y_vec = self.ode_solver.solve(new_system, self.vec_dim, self.t_span, vec_a.tolist(), h)
 
             ya = self.get_y_from_vec(y_vec[0])
             yb = self.get_y_from_vec(y_vec[len(y_vec) - 1])
-
-            # def make_graph():
-            #     plt.figure(figsize = [8,12])
-            #     plt.plot(x, list(map(lambda yn_vec: yn_vec[0], y_vec)))
-            #     plt.grid()
-            #     plt.show()
-
-            # make_graph()
 
             psi = self.bc(ya, yb)
 
@@ -146,23 +135,17 @@ class ShootingSolver(BaseBVPSolver):
 
             ga_jacobi, gb_jacobi = self.get_bc_jacobians(ya, yb, h_jacobi)
 
-            # print("ga_j = ", ga_jacobi)
-            # print("gb_j = ", gb_jacobi)
-            # print("va = ", va)
-            # print("vb = ", vb)
             v_1 = ga_jacobi @ va + gb_jacobi @ vb # v' матрица nxn
 
-            # print("v_1 = ", v_1)
-            # print("psi = ", psi)
             # сигнатура вызываемой фнукции:
             # def solve(self, A: Matrix, b: Vector) -> Vector:
             da = self.sla_solver.solve(Matrix(v_1), (-1) * Vector(psi))
 
             a = a + da
 
-            if verbose:
+            if record_alpha:
                 self.alpha_iters.append(i)
                 self.alphas.append(a)
+                self.last_iteration = i
 
-
-        raise RuntimeError("Решение не достигнето:(")
+        raise RuntimeError("Решение с точностью " + str(tol) + " за " + str(max_iter) + " итераций не достигнуто:(")
